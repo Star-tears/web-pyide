87ebfe28833c3bffe82bb452b04bfd6a8a17e5ae
Star-tears <star-tears@outlook.com>
2024-04-08 15:19:56 +0800

    init

---
+++ src/router/index.ts
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'VmIndex',
+      redirect: '/editor'
+    },
+    {
+      path: '/editor',
+      component: () => import('@/components/element/VmIde.vue'),
+      meta: { title: 'Vm Web IDE' }
+    }
+  ]
+})
+
+export default router
