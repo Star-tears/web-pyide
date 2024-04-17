bf06b05436dd75502a26c92bbce618240f8f73fc
Star-tears <star-tears@outlook.com>
2024-04-17 17:13:25 +0800

    feat: add xterm-fit

--- src/components/element/pages/ide/EdgeContainer.vue
+++ src/components/element/pages/ide/EdgeContainer.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="size-full">
-    <div id="xterm" class="xterm"></div>
+    <div id="xterm" class="xterm size-full"></div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { FitAddon } from '@xterm/addon-fit';
 import { Terminal } from '@xterm/xterm';
+
 onMounted(() => {
-  var term = new Terminal();
-  term.open(document.getElementById('xterm'));
-  term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
+  const terminal = new Terminal();
+  const fitAddon = new FitAddon();
+  terminal.loadAddon(fitAddon);
+  terminal.open(document.getElementById('xterm'));
+  fitAddon.fit();
+  terminal.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
 });
 </script>
 
