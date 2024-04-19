a9f427b3c83b7cc90c8449c57677b35146b43f9a
Star-tears <star-tears@outlook.com>
2024-04-19 10:55:12 +0800

    feat: add EdgeContainer MinBtn

--- src/components/element/pages/ide/EdgeContainer.vue
+++ src/components/element/pages/ide/EdgeContainer.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="size-full">
+  <div class="relative size-full">
+    <NButton
+      size="small"
+      class="absolute z-50 right-2 top-2 px-2"
+      :render-icon="renderMinIcon"
+      @click="onMinBtnClick"
+    ></NButton>
     <TerminalContainer v-if="ideInfo.edgeContainerValue === 'console'" />
     <MonitorContainer v-if="ideInfo.edgeContainerValue === 'monitor'" />
     <PkgManagerContainer v-if="ideInfo.edgeContainerValue === 'pkg-manager'" />
@@ -8,11 +14,21 @@
 
 <script setup lang="ts">
 import TerminalContainer from '@/components/element/pages/ide/edge-container/TerminalContainer.vue';
+import { NButton, NIcon } from 'naive-ui';
 import { useIdeStore } from '@/stores/ide';
 import { storeToRefs } from 'pinia';
+import { RemoveOutline } from '@vicons/ionicons5';
 
 const ideStore = useIdeStore();
 const { ideInfo } = storeToRefs(ideStore);
+const renderMinIcon = () => {
+  return h(NIcon, null, {
+    default: () => h(RemoveOutline)
+  });
+};
+const onMinBtnClick = () => {
+  ideInfo.value.edgeContainerValue = '';
+};
 </script>
 
 <style scoped></style>
