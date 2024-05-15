4e383696f834f0637344f2720fa13521dd7b82b8
Star-tears <star-tears@outlook.com>
2024-05-15 18:16:55 +0800

    fix: reload task error

--- server/app/utils/taskmanager.py
+++ server/app/utils/taskmanager.py
@@ -7,34 +7,34 @@ from typing import Any, Dict
 from app.utils.helper import singleton
 from fastapi import WebSocket
 
+
 @singleton
 class PythonConsoleConnectionManager:
     def __init__(self):
         self.active_connections: list[WebSocket] = []
-        self.active_connections_dict:Dict[str, list[WebSocket]]={}
-        self.queue_dict: Dict[str,asyncio.Queue]={}
-        self.single_queue_limit=200
+        self.active_connections_dict: Dict[str, list[WebSocket]] = {}
+        self.queue_dict: Dict[str, asyncio.Queue] = {}
+        self.single_queue_limit = 200
 
-    async def connect(self, websocket: WebSocket,taskId:str):
+    async def connect(self, websocket: WebSocket, taskId: str):
         await websocket.accept()
         self.active_connections.append(websocket)
-        self.add_websocket(taskId,websocket)
+        self.add_websocket(taskId, websocket)
 
-    async def put_message(self,taskId:str,message:str):
-        message_queue=self.queue_dict.setdefault(taskId,asyncio.Queue())
+    async def put_message(self, taskId: str, message: str):
+        message_queue = self.queue_dict.setdefault(taskId, asyncio.Queue())
         await message_queue.put(message)
-        current_size=message_queue.qsize()
-        if current_size>self.single_queue_limit:
+        current_size = message_queue.qsize()
+        if current_size > self.single_queue_limit:
             _ = await message_queue.get()
             message_queue.task_done()
-        
 
-    def disconnect(self, websocket: WebSocket,taskId:str):
+    def disconnect(self, websocket: WebSocket, taskId: str):
         self.active_connections.remove(websocket)
-        self.remove_websocket(taskId,websocket)
+        self.remove_websocket(taskId, websocket)
 
-    async def send_message(self,taskId):
-        message_queue=self.queue_dict.get(taskId)
+    async def send_message(self, taskId):
+        message_queue = self.queue_dict.get(taskId)
         if message_queue is None:
             await asyncio.sleep(1)
         else:
@@ -64,34 +64,38 @@ class PythonConsoleConnectionManager:
                 # 如果移除后列表变为空，则从字典中删除这个键
                 if not connections:
                     del self.active_connections_dict[taskId]
-    def remove_taskId(self,taskId,str):
+
+    def remove_taskId(self, taskId: str):
         # 获取对应taskId的列表，如果不存在则返回None
         message_queue = self.queue_dict.get(taskId)
         if message_queue is not None:
+            # print(f"del {taskId}")
             del self.active_connections_dict[taskId]
 
+
 pythonConsoleConnectionManager = PythonConsoleConnectionManager()
+
 
 @singleton
 class TaskManager(object):
     def __init__(self, *args, **kwargs) -> None:
         self.task_dict = {}
-    
+
     def get_task_id_list(self):
         return list(self.task_dict.keys())
-    
+
     def get_task_info_dict(self):
-        task_info_list:Dict[str,Dict[str,Any]]={}
+        task_info_list: Dict[str, Dict[str, Any]] = {}
         for task_id, sub_t in self.task_dict.items():
-            task_info_list[task_id] = {} 
-            task_info_list[task_id]["alive"]=sub_t.is_alive()
-            task_info_list[task_id]["cmd"]=sub_t.cmd
-        return task_info_list;
+            task_info_list[task_id] = {}
+            task_info_list[task_id]["alive"] = sub_t.is_alive()
+            task_info_list[task_id]["cmd"] = sub_t.cmd
+        return task_info_list
 
     def set_subprogram(self, program_id, sub_t):
-        self.kill_subprogram(program_id)
+        self.stop_subprogram(program_id)
         self.task_dict[program_id] = sub_t
-    
+
     def remove_subprogram(self, program_id):
         if program_id is None:
             self.stop_subprogram()
@@ -99,11 +103,11 @@ class TaskManager(object):
         elif program_id in self.task_dict:
             self.stop_subprogram(program_id)
             self.task_dict.pop(program_id, None)
-    
+
     def start_subprogram(self, program_id):
         if program_id in self.task_dict:
             self.task_dict[program_id].start()
-    
+
     def stop_subprogram(self, program_id):
         if program_id is None:
             for _, t in self.task_dict.items():
@@ -121,7 +125,7 @@ class TaskManager(object):
             except:
                 pass
 
-    def kill_subprogram(self,program_id):
+    def kill_subprogram(self, program_id):
         if program_id is None:
             for _, t in self.task_dict.items():
                 t.stop()
@@ -139,16 +143,19 @@ class TaskManager(object):
                 t.join()
             except:
                 pass
-    def reload_subprogram(self,program_id):
+
+    def reload_subprogram(self, program_id):
         if program_id in self.task_dict:
             try:
                 t = self.task_dict.get(program_id)
-                cmd=t.cmd
-                taskId=program_id
-                self.set_subprogram(program_id,SubProgramThread(cmd,taskId))
+                cmd = t.cmd
+                taskId = program_id
+                self.set_subprogram(program_id, SubProgramThread(cmd, taskId))
                 self.start_subprogram(program_id)
             except:
                 pass
+
+
 class SubProgramThread(threading.Thread):
     def __init__(self, cmd, id):
         super(SubProgramThread, self).__init__()
@@ -171,15 +178,23 @@ class SubProgramThread(threading.Thread):
         if stdout:
             try:
                 # print('[Program {} stdout: {}]'.format(self.id,stdout))
-                asyncio.run(pythonConsoleConnectionManager.put_message(self.id,stdout+'\r\n'))
+                asyncio.run(
+                    pythonConsoleConnectionManager.put_message(self.id, stdout + "\r\n")
+                )
             except Exception as e:
-                print('[Program {} is exception], {}'.format(self.id, e))
+                print("[Program {} is exception], {}".format(self.id, e))
+
     def run_program(self):
         p = None
-        print('[Program {} is start]'.format(self.id))
+        print("[Program {} is start]".format(self.id))
         try:
-            p = subprocess.Popen(self.cmd, shell=False, universal_newlines=True,
-                                 stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            p = subprocess.Popen(
+                self.cmd,
+                shell=False,
+                universal_newlines=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+            )
             self.p = p
             while self.alive and p.poll() is None:
                 stdout = p.stdout.readline()
@@ -187,9 +202,9 @@ class SubProgramThread(threading.Thread):
                 self.response_to_client(stdout)
                 time.sleep(0.01)
             if not self.alive:
-                self.response_to_client('[program is terminate]')
+                self.response_to_client("[program is terminate]")
                 p.kill()
-                print('[Program {} is terminate]'.format(self.id))
+                print("[Program {} is terminate]".format(self.id))
                 return
             try:
                 stdout = p.stdout.read()
@@ -198,16 +213,16 @@ class SubProgramThread(threading.Thread):
                 pass
             self.response_to_client(stdout)
             if p.returncode == 0:
-                print('Program {} success'.format(self.id))
+                print("Program {} success".format(self.id))
                 p.kill()
-                return 'ok'
+                return "ok"
             else:
-                print('Program {} failed'.format(self.id))
+                print("Program {} failed".format(self.id))
                 p.kill()
-                return 'failed'
+                return "failed"
         except Exception as e:
-            print('[Program {} is exception], {}'.format(self.id, e))
-            self.response_to_client('[Program is exception], {}'.format(e))
+            print("[Program {} is exception], {}".format(self.id, e))
+            self.response_to_client("[Program is exception], {}".format(e))
         finally:
             try:
                 p.kill()
