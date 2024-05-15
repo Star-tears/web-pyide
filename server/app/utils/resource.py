aa760554f05cb254694748598caf355bab50e2b8
Star-tears <star-tears@outlook.com>
2024-05-15 18:44:45 +0800

    fix: error when proj no .config

--- server/app/utils/resource.py
+++ server/app/utils/resource.py
@@ -22,7 +22,7 @@ CREATE_ERROR = -35
 class FilterRule(object):
     @staticmethod
     def filter(file):
-        if file in ['__pycache__'] or file.endswith(('.pyc', '.pyd', '.o')):
+        if file in ["__pycache__"] or file.endswith((".pyc", ".pyd", ".o")):
             return
         return file
 
@@ -30,7 +30,7 @@ class FilterRule(object):
 def read(path, is_json=False):
     if os.path.exists(path) and os.path.isfile(path):
         try:
-            with open(path, 'r', encoding='utf-8') as f:
+            with open(path, "r", encoding="utf-8") as f:
                 if is_json:
                     data = json.load(f)
                 else:
@@ -46,9 +46,17 @@ def read(path, is_json=False):
 
 def write(path, data, is_json=False):
     try:
-        with open(path, 'w', encoding='utf-8') as f:
+        with open(path, "w", encoding="utf-8") as f:
             if is_json:
-                json.dump(data, f, sort_keys=True, indent=4, skipkeys=True, separators=(',', ':'), ensure_ascii=False)
+                json.dump(
+                    data,
+                    f,
+                    sort_keys=True,
+                    indent=4,
+                    skipkeys=True,
+                    separators=(",", ":"),
+                    ensure_ascii=False,
+                )
             else:
                 f.write(data)
         return 0, None
@@ -102,50 +110,58 @@ def rename(old_path, new_path):
         return PATH_IS_NOT_EXIST, None
 
 
-def list_dir(path, result=None, prefix='', filter_config=True, list_type='simple'):
-    prefix = '' if prefix == '/' else prefix
+def list_dir(path, result=None, prefix="", filter_config=True, list_type="simple"):
+    prefix = "" if prefix == "/" else prefix
     if os.path.exists(path):
         if result is None:
             result = {
-                'uuid': '/',
-                'key': '/',
-                'name': os.path.basename(path),
-                'label': os.path.basename(path),
-                'type': 'dir',
+                "uuid": "/",
+                "key": "/",
+                "name": os.path.basename(path),
+                "label": os.path.basename(path),
+                "type": "dir",
                 # 'path': path,
-                'path': '/',
+                "path": "/",
             }
-        result['children'] = []
+        result["children"] = []
         for p in os.listdir(path):
             if filter_config:
-                if p == '.config':
+                if p == ".config":
                     continue
             if FilterRule.filter(p) is None:
                 continue
             abs_p = os.path.join(path, p)
             if os.path.isdir(abs_p):
                 _result = {
-                    'uuid': prefix + '/' + p,
-                    'key': prefix + '/' + p,
-                    'name': p,
-                    'label': p,
-                    'type': 'dir',
+                    "uuid": prefix + "/" + p,
+                    "key": prefix + "/" + p,
+                    "name": p,
+                    "label": p,
+                    "type": "dir",
                     # 'path': abs_p,
-                    'path': prefix + '/' + p,
+                    "path": prefix + "/" + p,
                 }
-                result['children'].append(_result)
-                if list_type == 'detail':
-                    list_dir(abs_p, _result, prefix=prefix + '/' + p, filter_config=False, list_type=list_type)
+                result["children"].append(_result)
+                if list_type == "detail":
+                    list_dir(
+                        abs_p,
+                        _result,
+                        prefix=prefix + "/" + p,
+                        filter_config=False,
+                        list_type=list_type,
+                    )
             else:
-                result['children'].append({
-                    'uuid': prefix + '/' + p,
-                    'key': prefix + '/' + p,
-                    'name': p,
-                    'label': p,
-                    'type': 'file',
-                    # 'path': abs_p,
-                    'path': prefix + '/' + p,
-                })
+                result["children"].append(
+                    {
+                        "uuid": prefix + "/" + p,
+                        "key": prefix + "/" + p,
+                        "name": p,
+                        "label": p,
+                        "type": "file",
+                        # 'path': abs_p,
+                        "path": prefix + "/" + p,
+                    }
+                )
     return result
 
 
@@ -158,23 +174,54 @@ def list_projects(path):
     projects = []
     if os.path.exists(path) and os.path.isdir(path):
         for prj in os.listdir(path):
-            _config_path = os.path.join(path, prj, '.config')
+            _config_path = os.path.join(path, prj, ".config")
             code, config_data = read(_config_path, is_json=True)
+            if code != 0:
+                if os.path.exists(_config_path):
+                    try:
+                        # 如果文件存在，则删除它
+                        os.remove(_config_path)
+                    except Exception as e:
+                        pass
+                config_data = {}
+                config_data["lastAccessTime"] = time.time()
+                code, _ = write(_config_path, config_data, is_json=True)
             if code == 0:
-                config_data['name'] = prj
-                config_data['atime'] = time.strftime("%d/%m/%Y %I:%M:%S %p",
-                                                     time.localtime(os.path.getatime(_config_path))),  # 访问时间
-                config_data['mtime'] = time.strftime("%d/%m/%Y %I:%M:%S %p",
-                                                     time.localtime(os.path.getmtime(_config_path))),  # 修改时间
-                config_data['ctime'] = time.strftime("%d/%m/%Y %I:%M:%S %p",
-                                                     time.localtime(os.path.getctime(_config_path))),  # 创建时间
+                config_data["name"] = prj
+                config_data["atime"] = (
+                    time.strftime(
+                        "%d/%m/%Y %I:%M:%S %p",
+                        time.localtime(os.path.getatime(_config_path)),
+                    ),
+                )  # 访问时间
+                config_data["mtime"] = (
+                    time.strftime(
+                        "%d/%m/%Y %I:%M:%S %p",
+                        time.localtime(os.path.getmtime(_config_path)),
+                    ),
+                )  # 修改时间
+                config_data["ctime"] = (
+                    time.strftime(
+                        "%d/%m/%Y %I:%M:%S %p",
+                        time.localtime(os.path.getctime(_config_path)),
+                    ),
+                )  # 创建时间
 
-                config_data['atime'] = config_data['atime'][0] if isinstance(
-                    config_data['atime'], tuple) else config_data['atime']
-                config_data['mtime'] = config_data['mtime'][0] if isinstance(
-                    config_data['mtime'], tuple) else config_data['mtime']
-                config_data['ctime'] = config_data['ctime'][0] if isinstance(
-                    config_data['ctime'], tuple) else config_data['ctime']
+                config_data["atime"] = (
+                    config_data["atime"][0]
+                    if isinstance(config_data["atime"], tuple)
+                    else config_data["atime"]
+                )
+                config_data["mtime"] = (
+                    config_data["mtime"][0]
+                    if isinstance(config_data["mtime"], tuple)
+                    else config_data["mtime"]
+                )
+                config_data["ctime"] = (
+                    config_data["ctime"][0]
+                    if isinstance(config_data["ctime"], tuple)
+                    else config_data["ctime"]
+                )
                 projects.append(config_data)
         return 0, projects
     elif not os.path.exists(path):
@@ -185,28 +232,46 @@ def list_projects(path):
 
 def get_project(path):
     if os.path.exists(path):
-        project = list_dir(path, list_type='detail')
-        _config_path = os.path.join(path, '.config')
+        project = list_dir(path, list_type="detail")
+        _config_path = os.path.join(path, ".config")
         _code, config_data = read(_config_path, is_json=True)
         if _code != 0:
             config_data = {}
-        config_data['lastAccessTime'] = time.time()
+        config_data["lastAccessTime"] = time.time()
         write(_config_path, config_data, is_json=True)
-        config_data['name'] = os.path.basename(path)
-        config_data['atime'] = time.strftime("%d/%m/%Y %I:%M:%S %p",
-                                             time.localtime(os.path.getatime(_config_path))),  # 访问时间
-        config_data['mtime'] = time.strftime("%d/%m/%Y %I:%M:%S %p",
-                                             time.localtime(os.path.getmtime(_config_path))),  # 修改时间
-        config_data['ctime'] = time.strftime("%d/%m/%Y %I:%M:%S %p",
-                                             time.localtime(os.path.getctime(_config_path))),  # 创建时间
+        config_data["name"] = os.path.basename(path)
+        config_data["atime"] = (
+            time.strftime(
+                "%d/%m/%Y %I:%M:%S %p", time.localtime(os.path.getatime(_config_path))
+            ),
+        )  # 访问时间
+        config_data["mtime"] = (
+            time.strftime(
+                "%d/%m/%Y %I:%M:%S %p", time.localtime(os.path.getmtime(_config_path))
+            ),
+        )  # 修改时间
+        config_data["ctime"] = (
+            time.strftime(
+                "%d/%m/%Y %I:%M:%S %p", time.localtime(os.path.getctime(_config_path))
+            ),
+        )  # 创建时间
 
-        config_data['atime'] = config_data['atime'][0] if isinstance(
-            config_data['atime'], tuple) else config_data['atime']
-        config_data['mtime'] = config_data['mtime'][0] if isinstance(
-            config_data['mtime'], tuple) else config_data['mtime']
-        config_data['ctime'] = config_data['ctime'][0] if isinstance(
-            config_data['ctime'], tuple) else config_data['ctime']
-        project['config'] = config_data
+        config_data["atime"] = (
+            config_data["atime"][0]
+            if isinstance(config_data["atime"], tuple)
+            else config_data["atime"]
+        )
+        config_data["mtime"] = (
+            config_data["mtime"][0]
+            if isinstance(config_data["mtime"], tuple)
+            else config_data["mtime"]
+        )
+        config_data["ctime"] = (
+            config_data["ctime"][0]
+            if isinstance(config_data["ctime"], tuple)
+            else config_data["ctime"]
+        )
+        project["config"] = config_data
         return 0, project
     else:
         return PATH_IS_NOT_EXIST, None
@@ -217,26 +282,26 @@ def create_project(path, config_data=None):
     if code == 0:
         if not isinstance(config_data, dict):
             config_data = {}
-        config_data['lastAccessTime'] = time.time()
-        code, _ = write(os.path.join(path, '.config'), config_data, is_json=True)
-        config_data['name'] = os.path.basename(path)
+        config_data["lastAccessTime"] = time.time()
+        code, _ = write(os.path.join(path, ".config"), config_data, is_json=True)
+        config_data["name"] = os.path.basename(path)
         return code, config_data
     else:
         return code, None
 
 
 def save_project(project_path, data):
-    _config_path = os.path.join(project_path, '.config')
+    _config_path = os.path.join(project_path, ".config")
     code, config_data = read(_config_path, is_json=True)
     if code != 0:
         config_data = {}
-    config_data['lastAccessTime'] = time.time()
+    config_data["lastAccessTime"] = time.time()
     expend_keys = data.expendKeys
     open_list = data.openList
     selectFilePath = data.selectFilePath
-    config_data['expendKeys'] = list(set(expend_keys))
-    config_data['openList'] = list(set(open_list))
-    config_data['selectFilePath'] = selectFilePath
+    config_data["expendKeys"] = list(set(expend_keys))
+    config_data["openList"] = list(set(open_list))
+    config_data["selectFilePath"] = selectFilePath
     write(_config_path, config_data, is_json=True)
     return code, config_data
 
@@ -244,11 +309,11 @@ def save_project(project_path, data):
 def get_project_file(project_path, file_path):
     code, data = read(file_path)
     if code == 0:
-        _config_path = os.path.join(project_path, '.config')
+        _config_path = os.path.join(project_path, ".config")
         _code, config_data = read(_config_path, is_json=True)
         if _code != 0:
             config_data = {}
-        config_data['lastAccessTime'] = time.time()
+        config_data["lastAccessTime"] = time.time()
         write(_config_path, config_data, is_json=True)
     return code, data
 
@@ -256,11 +321,11 @@ def get_project_file(project_path, file_path):
 def delete_project_file(project_path, file_path):
     code, _ = delete(file_path)
     if code == 0:
-        _config_path = os.path.join(project_path, '.config')
+        _config_path = os.path.join(project_path, ".config")
         _code, config_data = read(_config_path, is_json=True)
         if _code != 0:
             config_data = {}
-        config_data['lastAccessTime'] = time.time()
+        config_data["lastAccessTime"] = time.time()
         write(_config_path, config_data, is_json=True)
     return code, _
 
@@ -268,11 +333,11 @@ def delete_project_file(project_path, file_path):
 def rename_project_file(project_path, old_path, new_path):
     code, _ = rename(old_path, new_path)
     if code == 0:
-        _config_path = os.path.join(project_path, '.config')
+        _config_path = os.path.join(project_path, ".config")
         _code, config_data = read(_config_path, is_json=True)
         if _code != 0:
             config_data = {}
-        config_data['lastAccessTime'] = time.time()
+        config_data["lastAccessTime"] = time.time()
         write(_config_path, config_data, is_json=True)
     return code, _
 
@@ -280,11 +345,11 @@ def rename_project_file(project_path, old_path, new_path):
 def write_project_file(project_path, file_path, data):
     code, _ = write(file_path, data)
     if code == 0:
-        _config_path = os.path.join(project_path, '.config')
+        _config_path = os.path.join(project_path, ".config")
         _code, config_data = read(_config_path, is_json=True)
         if _code != 0:
             config_data = {}
-        config_data['lastAccessTime'] = time.time()
+        config_data["lastAccessTime"] = time.time()
         write(_config_path, config_data, is_json=True)
     return code, _
 
@@ -292,11 +357,10 @@ def write_project_file(project_path, file_path, data):
 def create_project_folder(project_path, folder_path):
     code, _ = create(folder_path)
     if code == 0:
-        _config_path = os.path.join(project_path, '.config')
+        _config_path = os.path.join(project_path, ".config")
         _code, config_data = read(_config_path, is_json=True)
         if _code != 0:
             config_data = {}
-        config_data['lastAccessTime'] = time.time()
+        config_data["lastAccessTime"] = time.time()
         write(_config_path, config_data, is_json=True)
     return code, _
-
