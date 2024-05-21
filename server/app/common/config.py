dfd73c3315773a70a6b3a6fbfa1993a7e6766efd
Star-tears <star-tears@outlook.com>
2024-05-20 22:44:56 -0700

    feat: add arg python-path

--- server/app/common/config.py
+++ server/app/common/config.py
@@ -1,12 +1,14 @@
-
 import os
+import sys
+
 
 class Config:
-    PYTHON = "python"
-    FRONTEND = os.path.join('dist')
-    WEBIDESERVER = os.path.join('.webide-server')
-    PROJECTS =  os.path.join('.webide-server','projects')
-    IDE = os.path.join('ide')
-    SDK = os.path.join('sdk')
+    # PYTHON = "python"
+    PYTHON = sys.executable
+    FRONTEND = os.path.join("dist")
+    WEBIDESERVER = os.path.join(".webide-server")
+    PROJECTS = os.path.join(".webide-server", "projects")
+    IDE = os.path.join("ide")
+    SDK = os.path.join("sdk")
     # PROJECTS = os.path.join(os.path.abspath('.'),'..','.webide-server', 'projects')
     # PROJECTS = os.path.join(os.path.dirname(__file__),'..','..','..','.webide-server', 'projects')
